2cb2f4430fdd31c7f6e4f3861748ea76a8d25674
Mio <m.iwase.23.5.933@4nonome.com>
2023-10-18 15:38:50 +0900

    storybookをアップデート

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,13 @@
 module.exports = {
   stories: ["../src/**/*.stories.jsx"],
   addons: ["@storybook/addon-essentials"],
+
   framework: {
     name: "@storybook/react-webpack5",
     options: {}
   },
+
+  docs: {
+    autodocs: true
+  }
 };
