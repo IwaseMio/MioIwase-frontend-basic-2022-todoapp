08ebc8847fbeaeaa4ce3e62ae6b2ced753585502
Mio <m.iwase.23.5.933@4nonome.com>
2023-10-18 15:35:55 +0900

    Atoms/Checkboxの作成

---
+++ src/components/Atoms/Checkbox/index.jsx
@@ -0,0 +1,39 @@
+import check from "../../../assets/svg/check.svg";
+import styled from "styled-components";
+import React from "react";
+import COLOR from "../../../variables/color";
+
+export const Checkbox = (props) => {
+  return (
+    <Button onClick={props.onClick}>
+      <div>
+        <img src={check} className="checkbox" />
+      </div>
+    </Button>
+  );
+};
+export default Checkbox;
+
+const Button = styled.button`
+  display: flex;
+  border: 2px solid ${COLOR.LIGHT_GRAY};
+  background: none;
+  border-radius: 2px;
+  width: 20px;
+  height: 20px;
+  padding: 0;
+  justify-content: center;
+  align-items: center;
+  :hover .checkbox{
+    display: block;
+    transition: 0.2s;
+    cursor: pointer;
+    }
+
+    .checkbox{
+        width: 16px;
+        height: 16px;
+        display: none;
+    }
+  }
+`;
