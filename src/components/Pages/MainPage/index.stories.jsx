54bd33f4cbc811401d153230f125ffcceda955cf
Mio <m.iwase.23.5.933@4nonome.com>
2023-12-10 11:14:07 +0900

    MainPageのindex.stories.jsx

--- src/components/Pages/MainPage/index.stories.jsx
+++ src/components/Pages/MainPage/index.stories.jsx
@@ -1,5 +1,20 @@
 import { MainPage } from "./index";
+import { AlertHandlerProvider } from "../../../contexts/alert_handler";
+import AlertManager from "../../Organisms/AlertManager";
 
 export default { component: MainPage };
 
-export const Default = {};
+const Template = (args) => <MainPage {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
+Default.decorators = [
+  (Story) => (
+    <div>
+      <AlertHandlerProvider>
+        <AlertManager />
+        <Story />
+      </AlertHandlerProvider>
+    </div>
+  ),
+];
