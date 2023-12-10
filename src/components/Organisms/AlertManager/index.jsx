50088e8b853b0756482f8b601d68643c5353967e
Mio <m.iwase.23.5.933@4nonome.com>
2023-12-10 11:13:17 +0900

    alertManager

---
+++ src/components/Organisms/AlertManager/index.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from "react";
+import { useAlertHandlerContext } from "../../../contexts/alert_handler";
+import Alert from "../../Atoms/Alert";
+import styled from "styled-components";
+
+const AlertManager = () => {
+  const AlertHandlerContext = useAlertHandlerContext();
+
+  useEffect(() => {
+    if (AlertHandlerContext.isActive === true) {
+      setTimeout(() => {
+        AlertHandlerContext.closeAlert();
+      }, 5000);
+    }
+  }, [AlertHandlerContext]);
+
+  return (
+    <AlertWrapper>
+      <Alert
+        isActive={AlertHandlerContext.isActive}
+        alertText={AlertHandlerContext.alertText}
+      />
+    </AlertWrapper>
+  );
+};
+
+export default AlertManager;
+
+const AlertWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
